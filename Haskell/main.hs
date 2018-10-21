87b3b66c62ab85867a24470e2778b9d9fb60e88e
GabrielAlmeidaAzevedo <gabriel.almeida.azevedo@ccc.ufcg.edu.br>
2018-10-20 21:25:36 -0300

    Refacts nos putStrLn e remoção de linhas vazias.

--- Haskell/main.hs
+++ Haskell/main.hs
@@ -11,41 +11,32 @@ computadorResponde = do
     putStrLn ("responde danada")
     
 
-
-
-
-
-
+start :: IO()
 start = do
     
-    
     let loop = do
-            putStrLn "==========================================================================================="
-            putStrLn "                                Bem vindo ao Equations Solver!";
-            putStrLn "==========================================================================================="
-            putStrLn "=========================================== MENU =========================================="
-            putStrLn "Escolha uma das opções abaixo:"
-            putStrLn "Modo Digitar equações (D)"
-            putStrLn "Modo Descobrir resultados (R)"
-            putStrLn "Encerrar programa (E)"
+        putStrLn ("===========================================================================================")
+        putStrLn ("                                Bem vindo ao Equations Solver!                             ")
+        putStrLn ("===========================================================================================")
+        putStrLn ("=========================================== MENU ==========================================")
+        putStrLn ("Escolha uma das opções abaixo:")
+        putStrLn ("Modo Digitar equações (D)")
+        putStrLn ("Modo Descobrir resultados (R)")
+        putStrLn ("Encerrar programa (E)")
 
+        opcao <- getLine   
 
-            opcao <- getLine   
+        if (opcao == "d") then computadorResponde else if (opcao == "r") then usuarioResponde else exitWith $ ExitFailure 3;
 
-            if (opcao == "d") then computadorResponde else if (opcao == "r") then usuarioResponde else exitWith $ ExitFailure 3;
+        let opInvalida = (opcao /= "r" && opcao /= "d" && opcao /= "e");
 
-            let opInvalida = (opcao /= "r" && opcao /= "d" && opcao /= "e");
+        if (opInvalida) then putStrLn "Opção inválida. Por favor tente novamente."
+        else putStrLn ""
+        
+        when (opInvalida) loop
+    loop
 
-            if (opInvalida) then putStrLn "Opção inválida. Por favor tente novamente."
-            else putStrLn ""
-            
-
-            
-            when (opInvalida) loop
-    loop  
-
-	
-    
+	   
 main = do
     start
 
