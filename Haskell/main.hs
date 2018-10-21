8662014977bbce39c33ce7130682cc0d721b5bc4
marcelovitorino <marcelo.vitorino@ccc.ufcg.edu.br>
2018-10-21 18:39:04 -0300

    adição de soluçãoLinear e calculaRaizes

--- Haskell/main.hs
+++ Haskell/main.hs
@@ -1,5 +1,7 @@
 import System.Exit
 import Data.Char
+import Control.Monad
+
 
 instrucoes :: IO()
 instrucoes = do
@@ -22,6 +24,39 @@ instrucoes = do
 	putStrLn ("d) x^2 + 5x/2 - 3/2 = 0 - É necessário ter espaços entre o termo e o /.");
 	putStrLn ("e) 3x^2 - 24x + 5 = -6x^2 - 11 -É necessário ter espaço entre o - e o 6.");
     putStrLn ("");
+
+
+    
+solucaoLinear :: Int -> Int -> IO()
+solucaoLinear a b = do  
+
+    if (a == 0) then putStrLn("A equação é linear porém a operação - b/a, gera uma divisão por zero, não possuindo solução.")
+    else do
+        let  x = fromIntegral((-1) * b) / fromIntegral(a)
+
+        putStrLn("A equação é linear e a sua solução é x = " ++ (show $ x))
+
+
+calculaRaizes :: Int -> Int -> Int -> IO()
+calculaRaizes a b c = do
+    
+    if (a == 0) then solucaoLinear b c
+    else do
+        let delta = (b * b) - (4 * a * c)
+        
+        if (delta < 0) then putStrLn("O delta ("++ (show $ delta) ++") é negativo, assim a equação não possui solução no conjunto dos números reais.")
+        else if (delta == 0) then do
+
+            let res = fromIntegral((-1) * b) / fromIntegral(2 * a)
+            putStrLn ("O delta é igual a zero, assim a equação possui duas soluções iguais a" ++ (show $ res) ++".)")
+        else do
+            let raiz = floor . sqrt $ fromIntegral delta
+            let x1 = fromIntegral(((-1) * b) + raiz) / fromIntegral(2 * a)
+            let x2 = fromIntegral(((-1) * b) - raiz) / fromIntegral(2 * a)
+            putStrLn ("Como o delta ("++ (show $ delta) ++") da quação possui duas soluções distintas. São elas: " ++ (show $ x1) ++ " e " ++ (show $ x2))
+            
+
+
 
 
 usuarioResponde :: IO()
