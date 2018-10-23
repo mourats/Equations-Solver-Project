4197dd1103d6c826dfaef2e322ffe41ac281504f
GabrielAlmeidaAzevedo <gabriel.almeida.azevedo@ccc.ufcg.edu.br>
2018-10-22 23:08:11 -0300

    Loops do usuarioResponde Okay e Refacts nos templates

--- Haskell/main.hs
+++ Haskell/main.hs
@@ -4,6 +4,8 @@ import qualified Validador as Validador
 import System.Exit
 import Data.Char
 import Control.Monad
+import System.Console.ANSI
+import Control.Concurrent
 
 
 lowerCase :: String -> String
@@ -31,7 +33,19 @@ instrucoes = do
 	putStrLn ("e) 3x^2 - 24x + 5 = -6x^2 - 11 -É necessário ter espaço entre o - e o 6.");
     putStrLn ("");
 
-
+mode :: String -> IO()
+mode opc = do
+    if (opc == "p")
+    then putStrLn ("======================== Modo equações do Primeiro Grau escolhido! ========================")
+    else if (opc == "s") then putStrLn ("========================= Modo equações do Segundo Grau escolhido! ========================")
+    else putStr ("")
+    if (opc == "p" || opc == "s") then do
+        putStrLn ("")
+        putStrLn ("Atenção:")
+        putStrLn ("As respostas são sempre um número (2, 5, -9, 2/3) ou V")
+        putStrLn ("V - Representa que o conjunto solução é vazio para o domínio dos Reais.")
+        putStrLn ("")
+    else putStr ("")
     
 solucaoLinear :: Int -> Int -> IO()
 solucaoLinear a b = do  
@@ -66,36 +80,64 @@ usuarioResponde :: IO()
 usuarioResponde = do
 
     let loop = do
-        putStrLn ("----Modo do usuário escolhido!----")
+        putStrLn ("=============================== Modo do usuário escolhido! ================================")
+        putStrLn ("")
         putStrLn ("Escolha um tipo de equação:")
         putStrLn ("Primeiro Grau (P)")
         putStrLn ("Segundo Grau (S)")
         putStrLn ("Caso deseje sair, digite <E>")
+        putStrLn ("")
 
         op <- getLine
         let operacao = lowerCase (op)
     
+        mode operacao
         if (operacao == "p") then do
-        putStrLn ("---- Modo equações do Primeiro Grau escolhido! ---- ")
-        putStrLn ("As respostas são sempre um número. Exemplos: (2, 5, -9, 2/3)")
-        -- Falta manipular a leitura com o split
-        let arq = Read.lerPrimeiroGrau
-        putStrLn (arq !! Read.randomValue (length arq))
-        else if (operacao == "s") then do            
-        putStrLn ("---- Modo equações do Segundo Grau escolhido! ---- ")
-        putStrLn ("As respostas são sempre um número (2, 5, -9, 2/3) ou V")
-        putStrLn ("V - Representa que o conjunto solução é vazio para o domínio dos Reais.")
-        -- Falta manipular a leitura com o split
-        let arq = Read.lerSegundoGrau
-        putStrLn (arq !! Read.randomValue (length arq))
+            let primeiroGrau = do
+                putStrLn ("Equação:")
+                -- Falta manipular a leitura com o split
+                let arq = Read.lerPrimeiroGrau
+                putStrLn (arq !! Read.randomValue (length arq))
+                putStrLn ("")
+                putStrLn ("Digite sua resposta")
+                let resposta = do
+                    resp <- getLine
+                    if (resp == "e") then exitWith $ ExitFailure 3
+                    else
+                        -- compara as respostas
+                        -- diz se acertou ou errou
+                        putStrLn ("");
+                resposta
+                threadDelay 1050000
+                primeiroGrau
+            primeiroGrau
+
+        else if (operacao == "s") then do
+            let segundoGrau = do
+                putStrLn ("Equação:")
+                -- Falta manipular a leitura com o split
+                let arq = Read.lerSegundoGrau
+                putStrLn (arq !! Read.randomValue (length arq))
+                putStrLn ("")
+                putStrLn ("Digite sua resposta")
+                let resposta = do
+                    resp <- getLine
+                    if (resp == "e") then exitWith $ ExitFailure 3
+                    else
+                        -- compara as respostas
+                        -- diz se acertou ou errou
+                        putStrLn ("");
+                resposta
+                threadDelay 1050000
+                segundoGrau
+            segundoGrau            
         else if (operacao == "e") then exitWith $ ExitFailure 3
         else do  
-            putStrLn ("Opção inválida. Por favor tente novamente.")
+            putStrLn ("Opção inválida. Por favor tente novamente.") 
             loop
     loop
 
     -- Leitura e exibição linha por linha
-
 
 computadorResponde :: IO()
 computadorResponde = do
@@ -117,7 +159,7 @@ computadorResponde = do
         if (equacao == "e") then exitWith $ ExitFailure 3 
         else if (equacao == "") then loopGetEquacao 
         else do
-            if (Validador.ehValida equacao) -- if(ehValida equacao)
+            if (Validador.ehValida equacao) 
             then do
                 let splitted = words equacao
                 let termosSomados = Validador.somarTermosComum splitted
@@ -142,13 +184,13 @@ start = do
 
     putStr (Read.logomarca)
 
-    putStrLn ("")
-    putStrLn ("===========================================================================================")
-    putStrLn ("                                Bem vindo ao Equations Solver!                             ")
-    putStrLn ("===========================================================================================")
-    putStrLn ("=========================================== MENU ==========================================")
-    putStrLn ("")
     let loop = do
+        putStrLn ("")
+        putStrLn ("===========================================================================================")
+        putStrLn ("                                Bem vindo ao Equations Solver!                             ")
+        putStrLn ("===========================================================================================")
+        putStrLn ("=========================================== MENU ==========================================")
+        putStrLn ("")
         putStrLn ("Escolha uma das opções abaixo:")
         putStrLn ("Modo Digitar equações (D)")
         putStrLn ("Modo Descobrir resultados (R)")
@@ -162,7 +204,9 @@ start = do
         else if (operacao == "e") then exitWith $ ExitFailure 3
         else do 
             putStrLn ("Opção inválida. Por favor tente novamente.")
-            putStrLn ("")            
+            putStrLn ("")  
+            threadDelay 1000000
+            clearScreen          
             loop
     loop
 
